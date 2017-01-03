e36f188f1dde20b4c8e3469282952789dec75298
Adriano A Costa <sdcomputadores@gmail.com>
2017-01-03 10:02:57 -0200

    Correção nos links

--- index.php
+++ index.php
@@ -4,11 +4,11 @@ echo "<h1>Índice</h1>";
 echo "<ul>" .
  "<li>Capítulo 2" .
  "<ul>" .
- "<li><a href=\"/cap-2/OneTrickConcrete.php\">classe abstrata</a></li>" .
- "<li><a href=\"/cap-2/ImplementAlhpa.php\">classe interface</a></li>" .
- "<li><a href=\"/cap-2/ConSQL.php\">classe interface constant</a></li>" .
- "<li><a href=\"/cap-2/UseProducts.php\">classe interface indução tipo</a></li>" .
- "<li><a href=\"/cap-2/PrivateVis.php\">Visibilidade privada</a></li>" .
+ "<li><a href=\"./cap-2/OneTrickConcrete.php\">classe abstrata</a></li>" .
+ "<li><a href=\"./cap-2/ImplementAlhpa.php\">classe interface</a></li>" .
+ "<li><a href=\"./cap-2/ConSQL.php\">classe interface constant</a></li>" .
+ "<li><a href=\"./cap-2/UseProducts.php\">classe interface indução tipo</a></li>" .
+ "<li><a href=\"./cap-2/PrivateVis.php\">Visibilidade privada</a></li>" .
  "</ul>" .
  "</li>" .
  "</ul>";
