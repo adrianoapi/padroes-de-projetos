c9939a0c68efd6cd5dd5e09c333b71ffaf5d18eb
Adriano A Costa <sdcomputadores@gmail.com>
2017-01-04 09:14:09 -0200

    Adição do capítulo 3

--- index.php
+++ index.php
@@ -1,19 +1,25 @@
 <?php
 
 echo "<h1>Índice</h1>";
-echo "<ul>" .
- "<li>Capítulo 2" .
+echo null .
  "<ul>" .
- "<li><a href=\"./cap-2/OneTrickConcrete.php\">classe abstrata</a></li>" .
- "<li><a href=\"./cap-2/ImplementAlhpa.php\">classe interface</a></li>" .
- "<li><a href=\"./cap-2/ConSQL.php\">classe interface constant</a></li>" .
- "<li><a href=\"./cap-2/UseProducts.php\">classe interface indução tipo</a></li>" .
- "<li><a href=\"./cap-2/PrivateVis.php\">Visibilidade privada</a></li>" .
- "<li><a href=\"./cap-2/ConcreteProtect.php\">Visibilidade protegida</a></li>" .
- "<li><a href=\"./cap-2/PublicVis.php\">Visibilidade pública</a></li>" .
- "<li><a href=\"./cap-2/GetSet.php\">Getters e Setters</a></li>" .
- "<li><a href=\"./cap-2/Client.php\">Herança</a></li>" .
- "<li><a href=\"./cap-2/Poly.php\">Polimorfismo</a></li>" .
- "</ul>" .
- "</li>" .
+ "  <li>Capítulo 2" .
+ "      <ul>" .
+ "          <li><a href=\"./cap-2/OneTrickConcrete.php\">classe abstrata</a></li>" .
+ "          <li><a href=\"./cap-2/ImplementAlhpa.php\">classe interface</a></li>" .
+ "          <li><a href=\"./cap-2/ConSQL.php\">classe interface constant</a></li>" .
+ "          <li><a href=\"./cap-2/UseProducts.php\">classe interface indução tipo</a></li>" .
+ "          <li><a href=\"./cap-2/PrivateVis.php\">Visibilidade privada</a></li>" .
+ "          <li><a href=\"./cap-2/ConcreteProtect.php\">Visibilidade protegida</a></li>" .
+ "          <li><a href=\"./cap-2/PublicVis.php\">Visibilidade pública</a></li>" .
+ "          <li><a href=\"./cap-2/GetSet.php\">Getters e Setters</a></li>" .
+ "          <li><a href=\"./cap-2/Client.php\">Herança</a></li>" .
+ "          <li><a href=\"./cap-2/Poly.php\">Polimorfismo</a></li>" .
+ "      </ul>" .
+ "  </li>" .
+ "  <li>Capítulo 3" .
+ "      <ul>" .
+ "          <li>aaa</li>" .
+ "      </ul>" .
+ "  </li>" .
  "</ul>";
