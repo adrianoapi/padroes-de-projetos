e7dcc33e1097b96be88680252cc22d2c08727e99
Adriano A Costa <sdcomputadores@gmail.com>
2017-01-16 12:18:05 -0200

    text product graphic alteração

--- cap-5/factory-alteracoes/GraphicProduct.php
+++ cap-5/factory-alteracoes/GraphicProduct.php
@@ -12,7 +12,7 @@ class GraphicProduct implements Product
         $this->mfgProduct = "<!doctype html><html><head><meta charset='UTF-8' />";
         $this->mfgProduct .= "<title>Map Factory</title>";
         $this->mfgProduct .= "</head><body>";
-        $this->mfgProduct .= "<img src = 'map.jpg' width='700' height='500' />";
+        $this->mfgProduct .= "<img style='padding: 10px 10px 10px 10px' src = 'map.jpg' width='700' height='500' />";
         $this->mfgProduct .= "</body></html>";
         return $this->mfgProduct;
     }
--- cap-5/factory-alteracoes/TextProduct.php
+++ cap-5/factory-alteracoes/TextProduct.php
@@ -9,7 +9,30 @@ class TextProduct implements Product
 
     public function getProperties()
     {
-        $this->mfgProduct = "This is text.";
+        $this->mfgProduct = NULL.
+                '<!doctype html>
+                <html>
+                    <head>
+                        <style type="text/css">
+                            header {
+                                color: #900;
+                                font-weight: bold;
+                                font-size: 24px;
+                                font-family: Verdana, Geneva, sans-serif;
+                            }
+                            p {
+                                font-family: Verdana, Genva, sans-serif;
+                                font-size: 12px;
+                            }
+                        </style>
+                        <meta charset="UTF-8">
+                        <title>Mali</title>
+                    </head>
+                    <body>
+                        <p>At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat.</p>
+                    </body>
+                </html>
+                ';
         return $this->mfgProduct;
     }
 
