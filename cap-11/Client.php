54dc192530521e141a97af47c2efb11b7aecdb37
Adriano A Costa <sdcomputadores@gmail.com>
2017-01-31 13:10:38 -0200

    finalizando o exemplo de login

--- cap-11/Client.php
+++ cap-11/Client.php
@@ -1,17 +1,28 @@
 <?php
 
-require_once 'UniversalConnect.php';
+include_once("Proxy.php");
 
-class ConnectClient
+class Client
 {
 
-    private $hookup;
+    private $proxy;
+    private $un;
+    private $pw;
 
     public function __construct()
     {
+        $this->tableMaster = "proxyLog";
         $this->hookup = UniversalConnect::doConnect();
+        $this->un = $this->hookup->real_escape_string(trim($_POST['uname']));
+        $this->pw = $this->hookup->real_escape_string(trim($_POST['pw']));
+        $this->getIface($this->proxy = new Proxy());
+    }
+
+    private function getIface(ISubject $proxy)
+    {
+        $proxy->login($this->un, $this->pw);
     }
 
 }
 
-$worker = new ConnectClient();
+$worker = new Client();
---
+++ cap-11/ConnectClient.php
@@ -0,0 +1,17 @@
+<?php
+
+require_once 'UniversalConnect.php';
+
+class ConnectClient
+{
+
+    private $hookup;
+
+    public function __construct()
+    {
+        $this->hookup = UniversalConnect::doConnect();
+    }
+
+}
+
+$worker = new ConnectClient();
